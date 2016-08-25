55848b4824db780decddf2fe971f8acaccc06b32
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-25 07:51:53 +0000

    iwp

--- hello_test.go
+++ hello_test.go
@@ -1,11 +1,5 @@
 package main
 
-func ExampleHello() {
-	cmdHello.Run(cmdHello, []string{})
-	// output:
-	// Hello []
-}
-
 func ExampleHelloWorld() {
 	cmdHello.Run(cmdHello, []string{"World"})
 	// output:
--- main_test.go
+++ main_test.go
@@ -6,9 +6,17 @@ import (
 
 func ExampleVersion() {
 	Version = "hello"
-	os.Args = []string{"codestand", "version"}
+	os.Args = []string{"cli", "version"}
 	main()
 
 	// Output:
 	// codestand/cli version hello
+}
+
+func ExampleHello() {
+	os.Args = []string{"cli", "hello"}
+	main()
+
+	// Output:
+	// Hello []
 }
